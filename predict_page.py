327cb7b62c8d4c20dc4ff281c89a157bbe8e87be
Ammar-theScorpion <assiammar692@gmail.com>
2022-12-08 00:23:33 +0200

    df

--- predict_page.py
+++ predict_page.py
@@ -8,6 +8,16 @@ import os
 
 image = Image.open('heart.jpeg')
 
+def predict(input_data):
+    input_array = np.asarray(input_data)
+    input_array = input_array.reshape(1, -1)
+    pickle_in = open("best.pickle", 'rb')
+    clf = pickle.load(pickle_in)
+    prediction = clf.predict(input_array)
+    if(prediction[0] == 0):
+        return 'Good'
+    else:
+        return 'Bad'
 
 
 class ML:
@@ -70,27 +80,32 @@ st.write('''### To predict your heart disease status''')
 st.write('''## 1- Enter the parameters that best describe you.''')
 st.write('''## 2- Press the "Predict" button and wait for the result.''')
 clicked_game = st.button('Play Game')
-st.sidebar.selectbox('Select your BMI', BMI)
-st.sidebar.selectbox('Select your Age', AGE)
-st.sidebar.selectbox('Select your Race', RACE)
-st.sidebar.selectbox('Select your Gender', GENDER)
-st.sidebar.selectbox('Have you smoked more than 100 cigarettes in your entire life ?)', YN)
-sleep = st.sidebar.select_slider(
+clicked_Predict = st.button('Predict')
+BMI = st.sidebar.selectbox('Select your BMI', BMI)
+AGE =st.sidebar.selectbox('Select your Age', AGE)
+RACE =st.sidebar.selectbox('Select your Race', RACE)
+GENDER =st.sidebar.selectbox('Select your Gender', GENDER)
+SMIKE =st.sidebar.selectbox('Have you smoked more than 100 cigarettes in your entire life ?)', YN)
+heal =sleep = st.sidebar.select_slider(
     'Select a color of the rainbow',
     options=['0', '1', '2', '3', '4', '5', '6'])
-st.sidebar.write('Hours of sleep per 24h', sleep)
-st.sidebar.selectbox('How many drinks of alcohol do you have in a week?', YN)
-st.sidebar.slider('Physical health in the past month (Excelent: 0 - Very bad: 30)', 0, 30, 2)
-st.sidebar.slider('Mental health in the past month (Excelent: 0 - Very bad: 30)', 0, 30, 2)
-st.sidebar.selectbox('Did you have a stroke?', YN)
-st.sidebar.selectbox('Physical activity in the past month', YN)
-st.sidebar.selectbox('Do you have serious difficulty walking or climbing stairs?', YN)
-st.sidebar.selectbox('Have you ever had diabetes?', YN)
-st.sidebar.selectbox('Do you have asthma?', YN)
-st.sidebar.selectbox('Do you have kidney disease?', YN)
-st.sidebar.selectbox('Do you have skin cancer?', YN)
+SLEEP = st.sidebar.write('Hours of sleep per 24h', sleep)
+DRINK =st.sidebar.selectbox('How many drinks of alcohol do you have in a week?', YN)
+Physical =st.sidebar.slider('Physical health in the past month (Excelent: 0 - Very bad: 30)', 0, 30, 2)
+Mental =st.sidebar.slider('Mental health in the past month (Excelent: 0 - Very bad: 30)', 0, 30, 2)
+STROKE = st.sidebar.selectbox('Did you have a stroke?', YN)
+Physicalactivity = st.sidebar.selectbox('Physical activity in the past month', YN)
+WALK = st.sidebar.selectbox('Do you have serious difficulty walking or climbing stairs?', YN)
+diabetes = st.sidebar.selectbox('Have you ever had diabetes?', YN)
+asthma = st.sidebar.selectbox('Do you have asthma?', YN)
+kidney = st.sidebar.selectbox('Do you have kidney disease?', YN)
+skin = st.sidebar.selectbox('Do you have skin cancer?', YN)
 
 if clicked_game:
-    st.write('''## 1- Enter the parameters that best describe you.''')
     os.system(os.path.join('.', 'main.exe'))
+data = ''
+if clicked_Predict:
+    data = predict([BMI, SMIKE, DRINK, STROKE, Physical, Mental, WALK, GENDER, AGE, RACE, diabetes, Physicalactivity, heal, SLEEP, asthma, kidney, skin])
+st.success(data)
+
 
